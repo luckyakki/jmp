39ef0b1e873be2437e5c220118b02eac82c76031
luckyakki <shaikrishna@inbox.com>
2015-07-11 17:58:04 -0700

    first commit

---
+++ src/main/java/com/luckymart/jmp/App.java
@@ -0,0 +1,13 @@
+package com.luckymart.jmp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
