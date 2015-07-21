bd26767eb65c96f70c6d6eb6e8b2a96dfcfa6d0f
luckyakki <shaikrishna@inbox.com>
2015-07-20 23:30:56 -0700

    Cricket class file has been changed

--- src/main/java/com/luckymart/jmp/Cricket.java
+++ src/main/java/com/luckymart/jmp/Cricket.java
@@ -2,6 +2,8 @@ package com.luckymart.jmp;
 
 public class Cricket {
 	String name; //member varible or instant variable//
+	String name1 = "Sachin Tendulkar";
+	String name2 = "Ricky Pointing";
 
 	public static void main(String[] args) {
 		Cricket sachin = new Cricket();
@@ -10,13 +12,26 @@ public class Cricket {
 		
 		Cricket ponti = new Cricket();
 		ponti.name ="ponti";
-		ponti.fielding();  }
+		ponti.fielding(); 
+		
+		Cricket obj = new Cricket();
+		obj.Batting();
+		
+	 }
 		
 	     public void Bat() {
 			System.out.println(name + " batting"); }
 			
 	     public void fielding(){
 			System.out.println(name + " fielding"); }
+	     
+	     public void Batting() {
+	    	 System.out.println("----------------------");
+				System.out.println(name1 + " batting"); 
+				System.out.println(name2 + " batting");		
+				System.out.println("----------------------");
+	     }
+	     
 			
 	     
 }
