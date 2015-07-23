e525abfb90261e934fb5c84ab012acf183e35532
luckyakki <shaikrishna@inbox.com>
2015-07-22 23:45:38 -0700

    refactored

---
+++ src/main/java/com/luckymart/jmp/Cricket1.java
@@ -0,0 +1,37 @@
+package com.luckymart.jmp;
+
+public class Cricket1 {
+
+	public static void main(String[] args) {
+		Cricket sachin = new Cricket();
+		Cricket pointy = new Cricket();
+		
+		sachin.Bat ();
+		pointy.fielding();
+	}
+	
+	public void Bat(String name){
+	System.out.println(name + "batting");
+	}
+	
+
+	public void fielding(String name){
+			System.out.println(name + "fielding");
+			}
+		
+		
+		}
+	
+	
+			
+			
+		
+		
+		
+		
+		
+		
+
+	
+
+
