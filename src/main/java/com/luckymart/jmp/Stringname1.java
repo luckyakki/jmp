099bb85fb7d671082e3368a3c080f37153a9ab24
luckyakki <shaikrishna@inbox.com>
2015-07-22 23:37:46 -0700

    use string to different methods

---
+++ src/main/java/com/luckymart/jmp/Stringname1.java
@@ -0,0 +1,25 @@
+package com.luckymart.jmp;
+
+public class Stringname1 {
+	public static void main(String[] args) {
+		int myint = 4;
+		
+		String text = "hello";
+		String  blank = "  ";
+		String  name = "lucky";
+		
+		String greeting = text + blank + name;
+		  
+		System.out.println("hello" + "  " + "lucky");
+		System.out.println(greeting);
+		System.out.println("my integer is "+   myint);
+		
+		double mybouble = 6.7;
+		System.out.println("my number is " + mybouble + ".");
+		
+     
+		
+		
+	}
+
+}
