0ab0c94ae4cb3a6246e18d92e08e57f6f1258560
luckyakki <shaikrishna@inbox.com>
2015-07-23 23:25:05 -0700

    while loop created

---
+++ src/main/java/com/luckymart/jmp/Loopwhile.java
@@ -0,0 +1,18 @@
+package com.luckymart.jmp;
+
+public class Loopwhile {
+	public static void main(String[] args) {
+		int value = 0;
+		
+		while(value<10)
+		{
+			
+		System.out.println("loop "  + value);
+		
+		value = value + 1;
+		
+		}
+		
+	}
+
+}
