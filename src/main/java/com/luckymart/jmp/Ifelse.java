56877283c77f5526df23084b10cdba23a73eb661
luckyakki <shaikrishna@inbox.com>
2015-07-27 00:20:51 -0700

    ifelse stmt created

---
+++ src/main/java/com/luckymart/jmp/Ifelse.java
@@ -0,0 +1,28 @@
+package com.luckymart.jmp;
+
+public class Ifelse {
+	public static void main(String[] args) {
+		int myint = 40;
+		
+		if(myint == 50){
+			System.out.println("this is true");
+			
+		}
+		else if(myint > 50){
+			System.out.println("this is false");
+			
+		}
+		else if (myint <50){
+			System.out.println("none of above");
+		}
+		else if (myint !=50){
+			System.out.println("this is correct");
+			
+		}
+		{
+			
+		}
+		
+	}
+
+}
