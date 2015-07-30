5dcbb70e99dacac3dfd16c34bf460889e50daa02
luckyakki <shaikrishna@inbox.com>
2015-07-30 00:28:41 -0700

    break created

---
+++ src/main/java/com/luckymart/jmp/Bbreak.java
@@ -0,0 +1,26 @@
+package com.luckymart.jmp;
+
+public class Bbreak {
+	public static void main(String[] args) {
+		int loop = 0;
+		while(loop<5){
+			System.out.println("looping " + loop);
+			loop++;
+			if (loop == 4){
+				
+				break;
+			}
+		loop++;
+		System.out.println("run");
+	
+		
+			}
+			
+		
+		
+		
+		
+	}
+	
+	
+}
