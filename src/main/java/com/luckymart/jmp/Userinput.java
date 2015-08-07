0bc9363c82b73516309107dbc136936acad6c888
luckyakki <shaikrishna@inbox.com>
2015-08-06 23:43:06 -0700

    userinput

--- src/main/java/com/luckymart/jmp/Userinput.java
+++ src/main/java/com/luckymart/jmp/Userinput.java
@@ -8,9 +8,9 @@ public class Userinput {
 		Scanner input = new Scanner(System.in);//create a scanner obj
 		
 		System.out.println("enter a line of text :");//output the prompt
-		String line = input.nextLine();//wait for the user to enter a line of text
+		String name = input.nextLine();//wait for the user to enter a line of text
 		
-		System.out.println("you entered: "+ line);//tell them to what they entered
+		System.out.println("you entered: "+ name);//tell them to what they entered
 		
 		
 		
