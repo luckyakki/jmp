f69644d5b620b400f79c4d9099276ab429d44e9c
luckyakki <shaikrishna@inbox.com>
2015-08-10 23:34:41 -0700

    increment oper

---
+++ src/main/java/com/luckymart/jmp/Incrementoperator.java
@@ -0,0 +1,34 @@
+package com.luckymart.jmp;
+
+public class Incrementoperator {              // increment operater ++ ,  --   //
+
+	public static void main(String[] args) {
+		
+		int a = 10;
+		System.out.println(a++);
+		
+	
+		int b = 10;
+		System.out.println(++b);  
+		
+		int c = 10;
+		System.out.println(c--);
+		                            
+		 int d = 10;                                                     // assignment oper 
+	     System.out.println(--d);	                                                   //  c -= 2 --> c=c-2
+				                                                   //  c *= 2 --> c=c*2
+				                                                    // c /= 2
+				                                                    // c %= 2
+				                                                    // c <<= 2 
+				                                                    // c >>= 2 
+				                                                   //  c >>= 2 
+				                                                    // c &= 2
+				                                                    // c ^= 2
+				                                                   //  c |= 2  
+		
+				
+
+	}
+}
+
+
