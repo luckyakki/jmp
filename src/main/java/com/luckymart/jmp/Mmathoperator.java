982a39be09583a7fe0f831ea605c5fd5a5b9f5bb
luckyakki <shaikrishna@inbox.com>
2015-08-10 23:35:56 -0700

    math operater

---
+++ src/main/java/com/luckymart/jmp/Mmathoperator.java
@@ -0,0 +1,41 @@
+package com.luckymart.jmp;
+
+public class Mmathoperator {
+	public static void main(String[] args)
+	{
+		int x,y,addition,subtraction,modulus;
+		x = 60;
+		y = 35;
+		
+		addition = x + y;
+	    System.out.println("addition = " + addition );
+		
+		subtraction = x - y;
+		System.out.println("subtraction = " + subtraction );
+		
+		modulus = x % y;
+		System.out.println("modulus = " + modulus );
+		
+        System.out.println("-----------------");  	
+		
+        double m,n,division;
+		m = 70;
+		n = 30;
+		
+		division = m / n;
+	    System.out.println("division = " + division );
+		
+		
+		// + ,-,*,/(division),%(modulus)  arithemetic operators//
+		
+		
+		
+		
+		
+
+		
+		
+		
+	}
+
+}
