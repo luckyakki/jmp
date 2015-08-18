5b760ffbf28bcd47e4595013b504b17d9b697775
luckyakki <shaikrishna@inbox.com>
2015-08-17 23:52:55 -0700

    method

---
+++ src/main/java/com/luckymart/jmp/Method.java
@@ -0,0 +1,15 @@
+package com.luckymart.jmp;
+
+public class Method {
+
+	public static void main(String[] args) {
+		methodname();
+		methodname();
+	}
+		public static void methodname(){
+		System.out.println("hello lucky");	
+		}
+		
+}
+
+
