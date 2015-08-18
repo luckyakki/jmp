d31ce36d25e67cc3bf81084e218c0f3396086a67
luckyakki <shaikrishna@inbox.com>
2015-08-17 23:54:12 -0700

    For loop

---
+++ src/main/java/com/luckymart/jmp/Forloopwitharray.java
@@ -0,0 +1,23 @@
+package com.luckymart.jmp;
+
+public class Forloopwitharray {
+
+	public static void main(String[] args) {
+		int[] myintarray = {100,60,50,40,30};
+		 
+	for(int index = 0; index < 5; index++ ) {      //for(intializing;expression;increment)//
+		                                       
+	  System.out.println(myintarray[index]);
+	
+		
+	}
+
+	}
+}
+
+
+
+/* intializing for loop in different way
+ * for (int element :myintarray){
+	System.out.println(element); 
+	}*/
