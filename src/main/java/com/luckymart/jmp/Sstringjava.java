24b8ba6dc9133e031aa148aec389a6fb1ce639f1
luckyakki <shaikrishna@inbox.com>
2015-08-17 23:54:56 -0700

    String

---
+++ src/main/java/com/luckymart/jmp/Sstringjava.java
@@ -0,0 +1,24 @@
+package com.luckymart.jmp;
+
+public class Sstringjava {
+                                           // string is sequence of character or string is character array //
+	public static void main(java.lang.String[] args) {
+		
+		String myString = "HELLO WORLD";
+		int myStringLength = myString.length(); // string length declaration //
+		String myStringinLowerCase = myString.toLowerCase(); //modify string to lower case //
+		 
+		System.out.println(myString);
+		System.out.println(myStringLength);
+		System.out.println(myStringinLowerCase);
+		System.out.println(myString.replace('E','A'));    //to replace a char ////
+		
+		System.out.println(myString.indexOf('E'));        // index //
+	
+		
+		
+			
+
+	}
+
+}
