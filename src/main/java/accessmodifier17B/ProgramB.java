6f58e0d61636377e93b09a31e6d240031f9ea6bd
luckyakki <shaikrishna@inbox.com>
2015-09-03 00:37:39 -0700

    Access modifier2

---
+++ src/main/java/accessmodifier17B/ProgramB.java
@@ -0,0 +1,13 @@
+package accessmodifier17B;
+                                                                // EJ Media --17 // 
+
+import accessmodifier17A.ClassTemplate;
+
+public class ProgramB{
+	public static void main(String[] args) {
+		ClassTemplate objt = new ClassTemplate();
+		System.out.println(objt.printweek());
+	}
+	}
+
+
