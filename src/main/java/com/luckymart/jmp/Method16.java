d993307f4209dc0a28cb91c154a1f006a0c4535b
luckyakki <shaikrishna@inbox.com>
2015-09-06 23:08:06 -0700

    method16

---
+++ src/main/java/com/luckymart/jmp/Method16.java
@@ -0,0 +1,15 @@
+package com.luckymart.jmp;
+
+public class Method16 {
+
+	public static void main(String[] args) {
+		methodname();
+		methodname();
+	}
+		public static void methodname(){
+		System.out.println("hello lucky");	
+		}
+		
+}
+
+
